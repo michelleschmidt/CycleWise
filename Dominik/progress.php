ee4d452ec16f565ef74b562abea465b0bc3e5dc5
chloe-1111 <94966003+chloe-1111@users.noreply.github.com>
2023-06-23 17:40:13 +0200

    Add files via upload

---
+++ Dominik/progress.php
@@ -0,0 +1,17 @@
+<!-- include header -->
+<?php
+  $pageTitle = 'Profile';
+  include_once( "templates/header.php" );
+?>
+
+<!-- content -->
+<div class="container">
+    <h2> Keep track of your progress! </h2><br>
+</div>
+
+
+<!-- include footer -->  
+<?php
+  $pageTitle = 'Profile';
+  include_once( "templates/footer2.php" );
+?>
